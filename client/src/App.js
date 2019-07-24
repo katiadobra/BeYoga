4e6ccef3b3dcea6e56c61d194ffde1ef8be22e12
katiadobra <katierybalko@gmail.com>
2019-07-24 16:33:47 +0200

    🐣 set-up react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>BeYoga</h1>
+        <p>Welcome page</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
